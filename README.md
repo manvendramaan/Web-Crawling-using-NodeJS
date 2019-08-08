7de9b24c99f4722c12bfdbbf6f671b1e6abf1a3a
manvendramaan <manvendra2215@gmail.com>
2019-08-08 12:17:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Web Crawling using NodeJS
+=========
+
+A small project which crawl the content of a website (https://medium.com/) to extract all the links on it along with its occurrences and params list and storing it in MongoDB.
+
+## How to run ?
+
+ 1. Install dependencies => npm i 
+ 2. Run it => node index 
+
+
+
---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const request = require('request');
+const HTMLParser = require('node-html-parser');
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/';
+
+app.get('/', function(req, res){
+	
+	const url = 'https://medium.com/';
+
+    request(url, function(error, response, body){
+		if(error)
+		{
+			console.error(error);
+			res.send('error',error);
+		}
+		let root = HTMLParser.parse(body);
+		let raw_data = root.querySelectorAll('a');
+		
+		const urls = [];
+		for(let i=0; i < raw_data.length; i++)
+		{
+			 urls.push(raw_data[i].rawAttrs.split('href="')[1].split('"')[0]);
+		}
+		
+		const urlsList = [];
+		
+		// extracting unique url along with its occurrences and params list
+		urls.forEach((url) => {
+			
+			if(urlsList.indexOf(url) <= -1)
+			{
+				urlsList[url] = {};
+				urlsList[url].count = 1;
+				try{
+					let actualUrl = new URL(url);
+					let search_params = new URLSearchParams(actualUrl.search); 
+					
+					let params = [];	
+					// iterate over the query parameters
+					for(let i of search_params) {
+						params.push(i[0]);
+					}
+					urlsList[url].params = params;
+				}catch(e)
+				{
+					console.error(e)
+				}
+			}else
+			{
+				urlsList[url].count = urlsList[url].count + 1;
+				try{
+					let actualUrl = new URL(url);
+					let search_params = new URLSearchParams(actualUrl.search); 
+	
+					// iterate over the query parameters
+					for(let i of search_params) {
+						if(urlsList[url].params.indexOf(i[0]) <= -1)
+						{
+							urlsList[url].params.push(i[0]);
+						}
+					}
+				}catch(e)
+				{
+					console.error(e)
+				}
+			}
+		})
+		
+		const formattedUrlsList = [];
+		
+		// formatting the urlsList to store in MongoDB
+		for(let urlList in urlsList)
+		{
+			formattedUrlsList.push({
+				url : urlList,
+				count : urlsList[urlList].count,
+				params : urlsList[urlList].params
+			})
+		}
+		
+		storingDataInMongoDB(formattedUrlsList);
+
+		res.send(formattedUrlsList)
+	});
+ 
+})
+
+function storingDataInMongoDB(dataArr)
+{
+	MongoClient.connect(url, function(err, db) {
+		
+	  if (err) throw err;
+	  
+	  const dbo = db.db("urlsList");
+	  
+	  dbo.collection("urls").insertMany(dataArr, function(err, res) {
+		  
+		if (err) throw err;
+		
+		console.log("Urls inserted");
+		
+		db.close();
+	  });
+	  
+	});
+}
+
+app.listen('3000')
+
+console.log('Server running on port 3000');
+
+exports = module.exports = app;
